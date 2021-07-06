35d9308d907452dcd3d3d902a72ee2f776826490
sangjun <sgbo5003@naver.com>
2021-07-06 14:19:07 +0900

    setting webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(c|sc|sa)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
