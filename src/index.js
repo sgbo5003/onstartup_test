ccdd245b0593c88edf4156ec289c18a03b74a607
sangjun <sgbo5003@naver.com>
2021-07-09 17:11:13 +0900

    change api url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
 // axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
-axios.defaults.baseURL = "http://15.164.227.114/web/src/php/get_info.php?";
+axios.defaults.baseURL =
+  "http://15.164.227.114/web/src/php/get_info.php?comment=1";
 
 ReactDOM.render(
   <React.StrictMode>
