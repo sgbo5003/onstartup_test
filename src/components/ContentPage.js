f47e184c782bb487d045ccd7acf5e029e92731a8
sangjun <sgbo5003@naver.com>
2021-07-13 15:22:20 +0900

    edit contentpage

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from "react";
 import "../css/content.css";
 import axios from "axios";
 import Content from "./Content";
+import Slider from "react-slick";
+// import "../css/_slick.css";
+import "../css/_slick-theme.css";
+import "../css/slick.css";
+import ScrollButton from "./ScrollButton";
+import viewIcon1 from "../images/view_icon1.png";
+import viewIcon2 from "../images/view_icon2.png";
 
 const ContentPage = () => {
-  const [mainData, setMainData] = useState({
+  const [data, setData] = useState({
     user_idx: [],
     user_name: [],
     user_belong: [],
@@ -14,114 +21,128 @@ const ContentPage = () => {
     comment_reply_num: [],
     comment_share_num: [],
     comment_save_num: [],
+    content_view_num: [],
   });
 
-  const [popularData, setPopularData] = useState({
-    user_idx: [],
-    user_name: [],
-    user_belong: [],
-    comment_time: [],
-    comment_text: [],
-    comment_like_num: [],
-    comment_reply_num: [],
-    comment_share_num: [],
-    comment_save_num: [],
-  });
-
-  function getMainData() {
-    return axios.get("/get_info.php?comment=1");
-  }
-
-  function getPopularData() {
-    return axios.get("/get_info.php?comment=2");
-  }
-
-  const getAllData = () => {
-    axios.all([getMainData(), getPopularData()]).then(
-      axios.spread((res1, res2) => {
-        console.log(res1);
-        console.log(res2);
-        setMainData(res1.data);
-        setPopularData(res2.data);
-      })
-    );
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: (
+      <button type="button" className="slick-prev">
+        Previous
+      </button>
+    ), // 이전 화살표 모양 설정
+    nextArrow: (
+      <button type="button" className="slick-next">
+        Next
+      </button>
+    ), // 다음 화살표 모양 설정
   };
-  // const getData = async () => {
-  //   const response = await axios.get("/web/src/php/get_info.php?comment=1");
-  //   console.log(response.data.user_name);
-  //   setData(response.data);
+
+  // const getAllData = () => {
+  //   axios.all([getMainData(), getPopularData()]).then(
+  //     axios.spread((res1, res2) => {
+  //       console.log(res1);
+  //       console.log(res2);
+  //       setMainData(res1.data);
+  //       setPopularData(res2.data);
+  //     })
+  //   );
   // };
 
+  const getData = async () => {
+    const response = await axios.get("/get_info.php?comment=4");
+    console.log(response.data.user_name);
+    setData(response.data);
+  };
+
   useEffect(() => {
-    // getData();
-    getAllData();
+    getData();
+    // getAllData();
   }, []);
 
-  const arr = [0, 1, 2, 3];
+  const arr = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24,
+  ];
 
   return (
-    <div class="wap home_wap">
-      <div class="home_content">
-        <div class="home_view">
+    <div className="wap home_wap">
+      <div className="home_content">
+        <div className="home_view">
           <h2>
             이 글 어때요?
-            <img src="src/images/view_icon1.png" alt="icon1.png" />
+            <img src={viewIcon1} alt="icon1.png" />
           </h2>
-          {arr.map((idx) => {
-            return (
-              <Content
-                key={mainData.user_idx[idx]}
-                title={mainData.user_name[idx]}
-                belong={mainData.user_belong[idx]}
-                commentTime={mainData.comment_time[idx]}
-                commentText={mainData.comment_text[idx]}
-                commentLikeNum={mainData.comment_like_num[idx]}
-                commentReplyNum={mainData.comment_reply_num[idx]}
-                commentShareNum={mainData.comment_share_num[idx]}
-                commentSaveNum={mainData.comment_save_num[idx]}
-              />
-            );
-          })}
+          <Slider {...settings}>
+            {arr.map((idx) => {
+              return (
+                <Content
+                  key={data.user_idx[idx]}
+                  title={data.user_name[idx]}
+                  belong={data.user_belong[idx]}
+                  commentTime={data.comment_time[idx]}
+                  commentText={data.comment_text[idx]}
+                  commentLikeNum={data.comment_like_num[idx]}
+                  commentReplyNum={data.comment_reply_num[idx]}
+                  commentShareNum={data.comment_share_num[idx]}
+                  commentSaveNum={data.comment_save_num[idx]}
+                />
+              );
+            })}
+          </Slider>
         </div>
-        <div class="home_view">
+        <div className="home_view">
           <h2>
             많은 사람들이 보고 있어요
-            <img src="src/images/view_icon2.png" alt="icon2.png" />
+            <img src={viewIcon2} alt="icon2.png" />
           </h2>
-          {arr.map((idx) => {
-            return (
-              <Content
-                key={popularData.user_idx[idx]}
-                title={popularData.user_name[idx]}
-                belong={popularData.user_belong[idx]}
-                commentTime={popularData.comment_time[idx]}
-                commentText={popularData.comment_text[idx]}
-                commentLikeNum={popularData.comment_like_num[idx]}
-                commentReplyNum={popularData.comment_reply_num[idx]}
-                commentShareNum={popularData.comment_share_num[idx]}
-                commentSaveNum={popularData.comment_save_num[idx]}
-              />
-            );
-          })}
+          <Slider {...settings}>
+            {arr
+              .slice(0)
+              .reverse()
+              .map((idx) => {
+                return (
+                  <Content
+                    key={data.user_idx[idx]}
+                    title={data.user_name[idx]}
+                    belong={data.user_belong[idx]}
+                    commentTime={data.comment_time[idx]}
+                    commentText={data.comment_text[idx]}
+                    commentLikeNum={data.comment_like_num[idx]}
+                    commentReplyNum={data.comment_reply_num[idx]}
+                    commentShareNum={data.comment_share_num[idx]}
+                    commentSaveNum={data.comment_save_num[idx]}
+                  />
+                );
+              })}
+          </Slider>
         </div>
-        <div class="home_view">
+        <div className="home_view">
           <h2>오늘의 NEW TOPIC</h2>
-          {arr.map((idx) => {
-            return (
-              <Content
-                key={mainData.user_idx[idx]}
-                title={mainData.user_name[idx]}
-                belong={mainData.user_belong[idx]}
-                commentTime={mainData.comment_time[idx]}
-                commentText={mainData.comment_text[idx]}
-                commentLikeNum={mainData.comment_like_num[idx]}
-                commentReplyNum={mainData.comment_reply_num[idx]}
-                commentShareNum={mainData.comment_share_num[idx]}
-                commentSaveNum={mainData.comment_save_num[idx]}
-              />
-            );
-          })}
+          <Slider {...settings}>
+            {arr.map((idx) => {
+              return (
+                <Content
+                  key={data.user_idx[idx]}
+                  title={data.user_name[idx]}
+                  belong={data.user_belong[idx]}
+                  commentTime={data.comment_time[idx]}
+                  commentText={data.comment_text[idx]}
+                  commentLikeNum={data.comment_like_num[idx]}
+                  commentReplyNum={data.comment_reply_num[idx]}
+                  commentShareNum={data.comment_share_num[idx]}
+                  commentSaveNum={data.comment_save_num[idx]}
+                />
+              );
+            })}
+          </Slider>
         </div>
+        <ScrollButton />
       </div>
     </div>
   );
