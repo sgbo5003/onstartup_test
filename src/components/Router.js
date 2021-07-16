d65e728eea923bd5e87bace3475fba89209db9fa
sangjun <sgbo5003@naver.com>
2021-07-16 09:28:12 +0900

    추가 & 조건부 렌더링

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, Route, Switch } from "react-router-dom";
 import Home from "../routes/Home";
 import Community from "../routes/Community";
 import Reference from "../routes/Reference";
@@ -9,11 +9,64 @@ import Join from "../routes/Join";
 import Header from "./Header";
 import SaveWrite from "../routes/SaveWrite";
 import Login from "../routes/Login";
+import defaultUserImg from "../images/default_user.png";
 
 const AppRouter = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  // function checkIsLogin() {
+  //   if (sessionStorage.getItem("user_email") === null) {
+  //     console.log("isLogin1 ?? :: ", isLogin);
+  //   } else {
+  //     setIsLogin(true);
+  //     console.log("isLogin2 ?? :: ", isLogin);
+  //   }
+  // }
+  function checkIsLogin() {
+    if (sessionStorage.length < 1) {
+      console.log("isLogin1 ?? :: ", isLogin);
+    } else {
+      setIsLogin(true);
+      console.log("isLogin2 ?? :: ", isLogin);
+    }
+  }
+
+  function headerIconTrue() {
+    return (
+      <h1 className="mypage_area">
+        <a className="mypage_photo_cove" href="my_page.php">
+          <img
+            className="mypage_photo"
+            src={defaultUserImg}
+            alt="default_user.png"
+          />
+        </a>
+      </h1>
+    );
+  }
+
+  function headerTextTrue() {
+    return (
+      <div className="coar_area">
+        <p>
+          <Link to="/Join">로그인</Link>
+        </p>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    checkIsLogin();
+    console.log("로그인 상태 :", isLogin);
+  }, [isLogin]);
+
   return (
     <>
-      <Header />
+      {isLogin ? (
+        <Header isLoginTrue={headerIconTrue()} />
+      ) : (
+        <Header isLoginTrue={headerTextTrue()} />
+      )}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/Community" component={Community} />
