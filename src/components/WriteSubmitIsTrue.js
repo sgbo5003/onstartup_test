5c8e50aedfa634a82b2820f6c2052584853d7d67
sangjun <sgbo5003@naver.com>
2021-07-20 18:46:16 +0900

    create submit component True & false

---
+++ src/components/WriteSubmitIsTrue.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const WriteSubmitIsTrue = (props) => {
+  const history = useHistory();
+  return (
+    <div class="write_comment_popup_cove_on">
+      <div class="write_comment_checking_popup">
+        <h3 class="write_comment_checking_title">코멘트를 등록했습니다.</h3>
+        <div>
+          <p>
+            <div
+              class="comment_checking_btn"
+              onClick={() => {
+                history.push("/");
+              }}
+            >
+              확인
+            </div>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WriteSubmitIsTrue;
