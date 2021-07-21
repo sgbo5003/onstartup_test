436b619219443a26a841612ec10deeda1bd70e06
sangjun <sgbo5003@naver.com>
2021-07-21 14:35:32 +0900

    드롭다운에 필요한 링크에 관련된 컴포넌트들

---
+++ src/routes/AccountManagement.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AccountManagement = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      About AccountManagement
+    </div>
+  );
+};
+
+export default AccountManagement;
