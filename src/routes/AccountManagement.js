c69a4e52537f2deeda500fe25a23ecae4efd67d5
sangjun <sgbo5003@naver.com>
2021-07-22 15:11:59 +0900

    계정관리화면 버튼 활성화 & 비활성화 기능 추가

--- src/routes/AccountManagement.js
+++ src/routes/AccountManagement.js
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AccountManagement = () => {
+  const [buttonOn, setButtonOn] = useState(false);
+  const [inputEmail, setInputEmail] = useState("");
+
+  const onSubmit = () => {
+    e.preventDefault();
+  };
+
+  function btnDeActivate() {
+    return (
+      <input
+        className="regit_account_submit_normal"
+        type="submit"
+        name="account_submit"
+        value="계정 정보 변경하기"
+        disabled
+      />
+    );
+  }
+
+  function btnActivate() {
+    return (
+      <input
+        className="regit_account_submit_active"
+        type="submit"
+        name="account_submit"
+        value="계정 정보 변경하기"
+      />
+    );
+  }
+
+  const onChangeInputEmail = (e) => {
+    setInputEmail(e.target.value);
+    console.log(e.target.value);
+  };
+
+  function checkBtnOn() {
+    if (inputEmail == "") {
+      setButtonOn(false);
+    } else {
+      setButtonOn(true);
+    }
+  }
+
+  // 버튼 변경 & state 변경 실시간 감지
+  useEffect(() => {
+    checkBtnOn();
+  });
   return (
     <div className="wap regit_pass_wap">
       <div className="regit_pass_content">
@@ -15,27 +62,13 @@ const AccountManagement = () => {
               name="new_email"
               maxlength="20"
               placeholder="이메일 입력"
+              onChange={onChangeInputEmail}
             />
             <div id="error_mail" className="result-email result-check"></div>
             <div id="error_tel" className="result-tel result-check"></div>
           </form>
-          <form
-            className="regit_account_submit_group"
-            onsubmit="return checkForm();"
-          >
-            <input
-              className="regit_account_submit_normal"
-              type="submit"
-              name="account_submit"
-              value="계정 정보 변경하기"
-              disabled
-            />
-            <input
-              className="regit_account_submit_active"
-              type="submit"
-              name="account_submit"
-              value="계정 정보 변경하기"
-            />
+          <form className="regit_account_submit_group" onSubmit={onSubmit}>
+            {buttonOn ? btnActivate() : btnDeActivate()}
           </form>
         </div>
       </div>
