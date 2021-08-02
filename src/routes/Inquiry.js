f2514c8284a4fe4ba90f32e1919770a1a86a19b4
sangjun <sgbo5003@naver.com>
2021-08-02 18:16:50 +0900

    문의하기 화면 생성

---
+++ src/routes/Inquiry.js
@@ -0,0 +1,120 @@
+import React from "react";
+import questionImg from "../images/question_tab_btn.png";
+import emailCheckedImg from "../images/email_checked.png";
+import emailCheckImg from "../images/email_check.png";
+import selectBackImg from "../images/select_back.png";
+
+const inquiry = () => {
+  return (
+    <div className="wap inquiry_wap">
+      <div className="inquiry_content">
+        <div className="inquiry_view">
+          <h2 className="inquiry_view_title">문의하기</h2>
+          <section className="inquiry_comment inq_sty">
+            <div className="inq_que_gruop">
+              <h2>자주 묻는 질문</h2>
+              <a className="question_tab" href="fav_question.php">
+                <img className="question_tab_img" src={questionImg} />
+              </a>
+            </div>
+          </section>
+          <section className="inquiry_comment2 inq_sty">
+            <div className="inq_que_gruop">
+              <h2 className="inq_title_wap">일반 문의</h2>
+              <form className="inq_form">
+                <input
+                  type="email"
+                  name="checked_email"
+                  placeholder="이메일 주소"
+                  maxlength="20"
+                  className="comment_inquiry_group inq_email_text inq_text_checked inquiry_text_box"
+                />
+                <div
+                  id="error_mail"
+                  className="result-email result-check"
+                ></div>
+              </form>
+            </div>
+            <div className="inq_que_group inq_select_group">
+              <form className="inq_form">
+                <input
+                  type="text"
+                  name="comment_select"
+                  placeholder="질문 유형 선택"
+                  disabled
+                  className="comment_inquiry_group inq_select_text inq_text_checked inquiry_text_box"
+                />
+                <a className="comment_select_img_cove" href="#">
+                  <img
+                    className="comment_select_img"
+                    src={selectBackImg}
+                    alt="select_back.png"
+                  />
+                </a>
+              </form>
+            </div>
+            <div className="inq_que_group">
+              <form className="inq_form_write">
+                <textarea
+                  type="text"
+                  name="checked_write"
+                  placeholder="내용"
+                  className="coment_inquiry inq_write_text inq_text_checked inquiry_text_box"
+                ></textarea>
+                <div
+                  id="error_write"
+                  className="result-write result-check"
+                ></div>
+              </form>
+            </div>
+          </section>
+          <section className="inquiry_comment3">
+            <form className="inq_check_email">
+              <a className="email_check_group">
+                <img
+                  className="email_check_off email_check_box"
+                  src={emailCheckImg}
+                  alt="email_check"
+                />
+                <img
+                  className="email_check_on email_check_box"
+                  src={emailCheckedImg}
+                  alt="email_checked"
+                />
+                <input
+                  className="email_check_val"
+                  type="text"
+                  value=""
+                  disabled
+                />
+              </a>
+              <p className="inq_text">
+                이메일 정보 제공 동의
+                <br />
+                <span>
+                  보내주신 질문에 답변드리기 위해 이메일 정보 제공에 동의해
+                  주시기 바랍니다.
+                </span>
+              </p>
+            </form>
+          </section>
+          <input
+            type="submit"
+            name="inq_submit_off"
+            className="inq_submit_off"
+            value="문의글 보내기"
+            disabled
+          />
+          <input
+            type="submit"
+            name="inq_submit_on"
+            className="inq_submit_on"
+            value="문의글 보내기"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default inquiry;
