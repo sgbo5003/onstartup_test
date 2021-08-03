a7b87976926e5fa320de74fcd802ad4bad398d9d
sangjun <sgbo5003@naver.com>
2021-08-03 17:41:24 +0900

    자료실 컨텐츠 부분 → 컨텐츠 컴포넌트 생성 (퍼블리싱)

---
+++ src/components/ReferenceContent/ReferenceContent.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Sidebar from "../Sidebar";
+import defaultUserImg from "../../images/default_user.png";
+import commentIcon from "../../images/icon_comment.png";
+import likeIcon from "../../images/icon_like.png";
+import likeOnIcon from "../../images/icon_like_on.png";
+import shareIcon from "../../images/icon_share.png";
+import messageBackIcon from "../../images/message_back.png";
+import saveIcon from "../../images/icon_sav.png";
+import saveOnIcon from "../../images/icon_sav_on.png";
+import imgExample1 from "../../images/port_gallery4.png";
+import ContextMenuIcon from "../../images/Context_menu_icon1.png";
+
+const ReferenceContent = () => {
+  return (
+    <div className="reference_content_home_tag_cove">
+      <div className="reference_content_img_container">
+        <img className="reference_content_img" src={imgExample1} />
+      </div>
+      <div className="reference_content_content_container">
+        <div className="reference_content_content_title_container">
+          <p>Title</p>
+          <p>Text</p>
+        </div>
+        <div className="reference_content_content_middle_container">
+          소제목 Lorem isupm dolor sit amet
+        </div>
+        <div className="reference_content_content_bottom_container">
+          <p>리뷰 12</p>
+          <div>
+            <img src={ContextMenuIcon} />
+            <span>00 코인</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReferenceContent;
