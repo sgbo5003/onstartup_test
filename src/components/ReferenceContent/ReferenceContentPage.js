7925672930a57bf5abbb670dfe65d9f74a2d3ece
sangjun <sgbo5003@naver.com>
2021-08-03 17:40:24 +0900

    자료실 컨텐츠 부분 컴포넌트 생성(퍼블리싱)

---
+++ src/components/ReferenceContent/ReferenceContentPage.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ScrollButton from "../ScrollButton";
+import viewIcon1 from "../../images/view_icon1.png";
+import viewIcon2 from "../../images/view_icon2.png";
+import ReferenceContent from "./ReferenceContent";
+
+const ReferenceContentPage = () => {
+  return (
+    <div className="reference_content">
+      <div className="reference_content_title_container">
+        <h2 className="reference_content_title_main">이 교재 어때요?</h2>
+        <img src={viewIcon1} alt="icon1.png" />
+      </div>
+      <div className="reference_content_contents_container">
+        <ReferenceContent />
+        <ReferenceContent />
+      </div>
+      <div className="reference_content_title_container">
+        <h2 className="reference_content_title_main">
+          많은 사람들이 보고 있어요
+        </h2>
+        <img src={viewIcon2} alt="icon2.png" />
+      </div>
+      <div className="reference_content_contents_container">
+        <ReferenceContent />
+        <ReferenceContent />
+      </div>
+      <div className="reference_content_title_container">
+        <h2 className="reference_content_title_main">NEW TOPIC</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ReferenceContentPage;
