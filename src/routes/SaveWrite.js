af0cfa5f9dcee2153e2fb6b3872bb7ea9ca2da6c
sangjun <sgbo5003@naver.com>
2021-08-05 09:25:25 +0900

    저장글 화면 퍼블리싱

--- src/routes/SaveWrite.js
+++ src/routes/SaveWrite.js
@@ -1,10 +1,35 @@
 import React from "react";
+import ContentComponent from "../components/ContentComponent";
+import Sidebar from "../components/Sidebar";
 
 const SaveWrite = () => {
+  const arr = [1, 2, 3, 4];
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      About SaveWrite
-    </div>
+    <>
+      <Sidebar />
+      <div className="content_savewrite">
+        <h2 className="savewrite_title_container">저장글</h2>
+        <div className="savewrite_content_container">
+          {arr.map((data) => {
+            return (
+              <ContentComponent
+                name="닉네임"
+                belong="소속"
+                title="title"
+                text="text"
+                updateDate="2021-08-04"
+                comment="댓글"
+                likeNum="1"
+                commentNum="2"
+                shareNum="3"
+                saveNum="4"
+                coverclass="middle_category_content_home_tag_cove"
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
