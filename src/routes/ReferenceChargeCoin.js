df5d453d6a143d1553ac578d30a6d789dca8d4cc
sangjun <sgbo5003@naver.com>
2021-08-05 09:26:35 +0900

    코인 충전하기 컴포넌트 생성

---
+++ src/routes/ReferenceChargeCoin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReferenceSidebar from "../components/ReferenceSidebar";
+
+const ReferenceChargeCoin = () => {
+  return (
+    <>
+      <ReferenceSidebar />
+      <div>hi</div>
+    </>
+  );
+};
+
+export default ReferenceChargeCoin;
