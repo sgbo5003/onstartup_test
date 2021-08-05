edf7e0e308b733b91715e5a3018b4432c48159d4
sangjun <sgbo5003@naver.com>
2021-08-05 09:26:17 +0900

    코인 선물받기 화면 퍼블리싱

---
+++ src/routes/ReferenceGiftCoin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReferenceSidebar from "../components/ReferenceSidebar";
+
+const ReferenceGiftCoin = () => {
+  return (
+    <>
+      <ReferenceSidebar />
+      <div>hi</div>
+    </>
+  );
+};
+
+export default ReferenceGiftCoin;
