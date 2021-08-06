66c911312d90218f9d07f4d27471c9b59471defb
sangjun <sgbo5003@naver.com>
2021-08-06 09:16:05 +0900

    자료실 -> 코인 선물받기 컴포넌트 생성

--- src/routes/ReferenceGiftCoin.js
+++ src/routes/ReferenceGiftCoin.js
@@ -5,7 +5,18 @@ const ReferenceGiftCoin = () => {
   return (
     <>
       <ReferenceSidebar />
-      <div>hi</div>
+      <div className="reference_gift_container">
+        <h1>코인 선물받기</h1>
+        <div className="reference_gift_code_container">
+          <h3 className="reference_gift_code_title">코드 입력</h3>
+          <div className="reference_gift_code_form_container">
+            <input type="text" />
+            <button type="submit" className="reference_gift_code_form_button">
+              등록하기
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
