505d62edddf79e99678a26c561fa387d6a0a33a9
sangjun <sgbo5003@naver.com>
2021-08-06 09:15:12 +0900

    component mapping

--- src/routes/ReferenceStorageBox.js
+++ src/routes/ReferenceStorageBox.js
@@ -3,18 +3,16 @@ import ReferenceContent from "../components/ReferenceContent/ReferenceContent";
 import ReferenceSidebar from "../components/ReferenceSidebar";
 
 const ReferenceStorageBox = () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       <ReferenceSidebar />
       <div className="reference_storagebox_content">
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
-        <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
+        {arr.map((data) => {
+          return (
+            <ReferenceContent coinbox="reference_content_content_bottom_container_coinbox" />
+          );
+        })}
       </div>
     </>
   );
